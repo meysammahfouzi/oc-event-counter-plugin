d4b86f11171f435fd7ba6e440b2bf4ec537ca11c
Meysam Mahfouzi <maysam.mahfouzi@gmail.com>
2019-07-21 08:25:57 +0430

    Add checking events table exists in boot method of plugin

--- Plugin.php
+++ Plugin.php
@@ -3,31 +3,31 @@
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Schema;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
-    public $elevated = true;
-
     public function boot()
     {
+        if (Schema::hasTable('meysam_eventcounter_events')) {
+            $events = DB::table('meysam_eventcounter_events')->select('id', 'plugin_name', 'event_name')->get();
 
-        $events = DB::table('meysam_eventcounter_events')->select('id', 'plugin_name', 'event_name')->get();
-
-        // Listen to events and log them as they happen
-        foreach ($events as $event) {
-            // TODO: Why is $event->full_plugin_name not recognized here?
-            $full_plugin_name = $event->plugin_name . '.' . $event->event_name;
-            Event::listen($full_plugin_name, function() use($event) {
-                Db::table('meysam_eventcounter_event_logs')->insert(
-                    [
-                        'event_id' => $event->id,
-                        'date' => Carbon::now()->format("Y/m/d"),
-                        'created_at' => Carbon::now(),
-                        'updated_at' => Carbon::now()
-                    ]
-                );
-            });
+            // Listen to events and log them as they happen
+            foreach ($events as $event) {
+                // TODO: Why is $event->full_plugin_name not recognized here?
+                $full_plugin_name = $event->plugin_name . '.' . $event->event_name;
+                Event::listen($full_plugin_name, function () use ($event) {
+                    Db::table('meysam_eventcounter_event_logs')->insert(
+                        [
+                            'event_id' => $event->id,
+                            'date' => Carbon::now()->format("Y/m/d"),
+                            'created_at' => Carbon::now(),
+                            'updated_at' => Carbon::now()
+                        ]
+                    );
+                });
+            }
         }
     }
 
