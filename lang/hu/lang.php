017715adbb7ebe814fcecc9ca94bf3d0df4a565e
Szabó Gergő <gergo85@gmail.com>
2019-10-07 10:42:03 +0200

    Add Hungarian translation

---
+++ lang/hu/lang.php
@@ -0,0 +1,14 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Esemény számláló',
+        'description' => 'Diagramos megjelenítés meghatározott események számára.',
+    ],
+    'event' => [
+        'name' => 'Esemény neve',
+        'plugin_name' => 'Bővítmény neve',
+        'tracked_plugin_name' => 'Bővítmény neve',
+        'manage_events' => 'Események kezelése',
+        'events' => 'Események',
+        'date_time' => 'Esemény dátuma',
+    ],
+];
